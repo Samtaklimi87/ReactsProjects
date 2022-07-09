2a18375807e82e58abb72e0de05c3f8cb8e7abfb
Sam Taklimi <sam.taklimi87@gmail.com>
2022-07-09 13:57:00 +0200

    fixing bug to be able to add items , adding submit items

--- src/App.js
+++ src/App.js
@@ -23,25 +23,45 @@ function App() {
       item: "Item 3",
     },
   ]);
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppingList", JSON.stringify(newItems));
+  }
+ const [newItem , setNewItem] = useState('')
+ const addItem = (item) => {
+    const id = items.length ? items[items.length-1].id +1 : 1;
+    const myNewItem = {id, checked: false ,item};
+    const listItems = [...items , myNewItem];
+    setAndSaveItems(listItems);
+ }
 
   const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) return;
+    // console.log(newItem);
+    addItem(newItem);
+    setNewItem('');
+  }
 
   return (
     <div className="App">
       <Header title="Grocery List" />
-      <AddItem />
+      <AddItem 
+          //adding props to additem
+          newItem={newItem}
+          setNewItem={setNewItem}
+          handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         setItems={setItems}
